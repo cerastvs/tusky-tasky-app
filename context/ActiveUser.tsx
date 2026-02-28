484150d35c0ed29a1027c301faf56392a7a3fd82
cerastvs <yuriksoliman@gmail.com>
2026-02-28 09:56:33 +0800

    refactor: improve ActiveUser context

--- context/ActiveUser.tsx
+++ context/ActiveUser.tsx
@@ -1,4 +1,23 @@
-import { createContext } from "react";
-let activeUser = createContext(1);
+"use client";
 
-export default activeUser;
+import { createContext } from "react";
+
+type User = {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+};
+
+export const ActiveUser = createContext<User | null>(null);
+
+type Props = {
+  children: React.ReactNode;
+  user: User | null;
+};
+
+export default function ActiveUserProvider({ user, children }: Props) {
+  return <ActiveUser.Provider value={user}>{children}</ActiveUser.Provider>;
+}
